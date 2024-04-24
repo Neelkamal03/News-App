2bb53ac6aecfbe7866c3e8b35670d4bdbf25ee40
neelkamal <kneel062@gmail.com>
2024-04-24 16:24:48 +0530

    fetching real time data from api, loop the api data

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem;" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card" style={{ width: "25rem" }}>
+          <img src={imageUrl ? imageUrl : "https://media.istockphoto.com/id/1313303632/video/breaking-news-template-intro-for-tv-broadcast-news-show-program-with-3d-breaking-news-text.jpg?s=640x640&k=20&c=S0dTZp37XKVcCAnoguMnRatvv4Nkp2cjmA5aYOOrJs8="} className="card-img-top" style={{ height: "250px", objectFit: "cover" }} alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newDetail" className="btn btn-primary">Read More Details</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-primary">Read More Details</a>
           </div>
         </div>
       </div>
